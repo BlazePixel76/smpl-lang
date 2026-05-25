dc24c167e0852f22ec08e17c11a96700459d695a
Reyad Hassan Takialdin <rhextra23@gmail.com>
2026-05-25 19:57:16 +0400

    yes

--- smpl.py
+++ smpl.py
@@ -8,7 +8,7 @@ from typing import Any, Dict, List, Tuple
 if sys.platform == "win32":
     sys.stdout.reconfigure(encoding="utf-8")
 
-VERSION = "0.1.0"
+VERSION = "1.0.0"
 
 # Token Types
 TOKEN_SET = "SET"
